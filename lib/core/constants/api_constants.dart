cefbabeca111461089309ebe9ef4f0a0af9c79b1
Furk4nBulut <furkanbtng@gmail.com>
2025-07-02 13:36:57 +0300

    API constantly fetched from .env

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,1 +1,6 @@
-// API base url ve endpoint sabitleri burada tanımlanacak 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class ApiConstants {
+  static String get baseUrl => dotenv.env['API_BASE_URL'] ?? '';
+  static String get apiKey => dotenv.env['API_KEY'] ?? '';
+} 
